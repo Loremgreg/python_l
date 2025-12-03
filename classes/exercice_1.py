f5ff9826d8af1516243e06c78d6303deae9e0519
Loremgreg <gregorybastin.de@gmail.com>
2025-12-03 16:26:42 +0100

    books

---
+++ classes/exercice_1.py
@@ -0,0 +1,23 @@
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.pages})"
+    
+    def is_long(self):
+        if self.pages > 300:
+            return True
+        else:
+            return False
+
+def main():
+    book = Book("1984", "George Orwell", 350)
+    print(book)
+    print(book.is_long())
+
+
+if __name__ == "__main__":
+    main()
